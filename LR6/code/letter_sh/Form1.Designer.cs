fab775063c6563ef880d0e3a4976ad2f3eaffa72
vasilevskykv <42437106+vasilevskykv@users.noreply.github.com>
2023-05-11 12:28:55 +0300

    Added LR6

---
+++ LR6/code/letter_sh/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace letter_sh
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.glControl1 = new OpenTK.GLControl();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // glControl1
+            // 
+            this.glControl1.BackColor = System.Drawing.Color.Black;
+            this.glControl1.Location = new System.Drawing.Point(12, 12);
+            this.glControl1.Name = "glControl1";
+            this.glControl1.Size = new System.Drawing.Size(531, 397);
+            this.glControl1.TabIndex = 0;
+            this.glControl1.VSync = false;
+            this.glControl1.Paint += new System.Windows.Forms.PaintEventHandler(this.glControl1_Paint);
+            this.glControl1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.glControl1_MouseMove);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(12, 420);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Ось X красная";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.Green;
+            this.label2.Location = new System.Drawing.Point(100, 420);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Ось Y зеленая";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.label3.Location = new System.Drawing.Point(191, 423);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Ось Z - синяя";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(555, 456);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.glControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private OpenTK.GLControl glControl1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ LR6/code/letter_sh/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
+
+namespace letter_sh
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            GL.ClearColor(0.3f, 0.3f, 0.3f, 1);
+
+            GL.MatrixMode(MatrixMode.Projection);
+            GL.LoadIdentity();
+
+            Matrix4 m;
+
+            m =
+                Matrix4.LookAt(0, 1, 3, 0, 0, 0, 0, 1, 0) *
+                Matrix4.CreatePerspectiveFieldOfView((float)(60 * Math.PI / 180.0), 1, 0.1f, 10);
+            GL.LoadMatrix(ref m);
+        }
+
+        Vector3[] vert = new Vector3[] {       
+            // Size 9,117187 x 8,58984375
+
+            // Start point
+            new Vector3( 0f, 0f, 0 ),
+            // PolyLineSegment
+           
+        };
+
+        void drawAxes()
+        {
+            // Рисуем оси
+            GL.Begin(PrimitiveType.Lines);
+
+            GL.Color3(1.0f, 0, 0);
+            GL.Vertex3(0, 0, 0);
+            GL.Vertex3(15, 0, 0);
+
+            GL.Color3(0, 1.0f, 0);
+            GL.Vertex3(0, 0, 0);
+            GL.Vertex3(0, 15, 0);
+
+            GL.Color3(0, 0, 1.0f);
+            GL.Vertex3(0, 0, 0);
+            GL.Vertex3(0, 0, 15); // */
+
+            GL.End();
+        }
+
+        float ra = 0, rb = 135;
+
+        private void glControl1_Paint(object sender, PaintEventArgs e)
+        {
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+
+            GL.MatrixMode(MatrixMode.Modelview);
+
+            Matrix4 m = 
+                Matrix4.CreateTranslation( -4.5f, -4.4f, 0 ) *
+                Matrix4.CreateRotationX(rb) *
+                Matrix4.CreateRotationY(ra) * 
+                Matrix4.CreateScale( 0.2f, 0.2f, 0.2f );
+
+            GL.LoadMatrix(ref m);
+
+            drawAxes();
+
+            GL.Color3(0, 1.0f, 0);
+
+            GL.Begin(PrimitiveType.Lines);
+
+            
+            GL.End();
+
+            glControl1.SwapBuffers();
+        }
+
+        int oldmx, oldmy;
+
+        private void glControl1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == System.Windows.Forms.MouseButtons.Left)
+            {
+                ra += (e.X - oldmx) / 100.0f;
+                rb += (e.Y - oldmy) / 100.0f;
+
+                glControl1.Invalidate();
+            }
+
+            oldmx = e.X;
+            oldmy = e.Y;
+        }
+    }
+}
